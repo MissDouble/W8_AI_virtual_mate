6143e12aed52180d1a24d57ab5a3e726133314d3
DoDo <doubleplus37@gmail.com>
2023-09-10 02:13:54 +0800

    rename repo base

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/W8_AIVirtualMate/',
+  base: '/W8_AI_virtual_mate/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
